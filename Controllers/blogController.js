4cd16ea34b01b4ad6e66772ba6f6a315242f8032
dolapo-mosuro <mosurodolapo@gmail.com>
2023-10-30 11:19:05 +0100

    new

--- Controllers/blogController.js
+++ Controllers/blogController.js
@@ -1,42 +1,42 @@
-// const blogModel = require("./models/blogs");
-// const router = express.Router;
-// const express = require("express");
+const blogModel = require("./models/blogs");
+const router = express.Router;
+const express = require("express");
 
-// // Get a list of blogs
-// const getBlogs = async (req, res) => {
-// 	// Implementation to retrieve a list of blogs
-// 	try {
-// 		const blogs = await blogModel.find();
-// 		res.render("index", { blogs });
-// 	} catch (error) {
-// 		res.status(500).json({ message: "Internal Server Error" });
-// 	}
-// };
+// Get a list of blogs
+const getBlogs = async (req, res) => {
+	// Implementation to retrieve a list of blogs
+	try {
+		const blogs = await blogModel.find();
+		res.render("index", { blogs });
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error" });
+	}
+};
 
-// // Get a specific blog by ID
-// const getBlogById = async (req, res) => {
-// 	// Implementation to retrieve a specific blog by ID
-// };
-// l;
-// // Create a new blog
-// const createBlog = async (req, res) => {
-// 	// Implementation to create a new blog
-// };
+// Get a specific blog by ID
+const getBlogById = async (req, res) => {
+	// Implementation to retrieve a specific blog by ID
+};
+l;
+// Create a new blog
+const createBlog = async (req, res) => {
+	// Implementation to create a new blog
+};
 
-// // Update an existing blog
-// const updateBlog = async (req, res) => {
-// 	// Implementation to update an existing blog
-// };
+// Update an existing blog
+const updateBlog = async (req, res) => {
+	// Implementation to update an existing blog
+};
 
-// // Delete a blog
-// const deleteBlog = async (req, res) => {
-// 	// Implementation to delete a blog
-// };
+// Delete a blog
+const deleteBlog = async (req, res) => {
+	// Implementation to delete a blog
+};
 
-// module.exports = {
-// 	createBlog,
-// 	updateBlog,
-// 	deleteBlog,
-// 	getBlogs,
-// 	getBlogById,
-// };
+module.exports = {
+	createBlog,
+	updateBlog,
+	deleteBlog,
+	getBlogs,
+	getBlogById,
+};
